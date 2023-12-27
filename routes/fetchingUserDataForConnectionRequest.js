892174f28029f002ae06c4c190c268857e283cdd
unknown <prabhjotarora99@gmail.com>
2023-12-27 22:04:03 +0530

    Fixing buga

--- routes/fetchingUserDataForConnectionRequest.js
+++ routes/fetchingUserDataForConnectionRequest.js
@@ -2,21 +2,24 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/users");
 
-router.get("/users/:id", async (req, res) => {
-  const userId = req.params.id;
+router.get(
+  "https://socio-app-y5og.onrender.com/users/:id",
+  async (req, res) => {
+    const userId = req.params.id;
 
-  try {
-    const user = await User.findById(userId);
+    try {
+      const user = await User.findById(userId);
 
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.json(user);
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
-
-    res.json(user);
-  } catch (error) {
-    console.error("Error fetching user:", error);
-    res.status(500).json({ message: "Internal Server Error" });
   }
-});
+);
 
 module.exports = router;

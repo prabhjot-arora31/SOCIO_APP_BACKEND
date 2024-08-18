59937cd449ee2bbbaf703872f6520a7dfe95652c
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-08-18 07:52:51 +0530

    Update chatsRoute.js

--- routes/chatsRoute.js
+++ routes/chatsRoute.js
@@ -4,16 +4,7 @@ const User = require("../model/users");
 
 router.get(
   "/chats",
-  async (req, res, next) => {
-    if (req.session && req.session.user) {
-      // Assuming session-based authentication
-      // User is authenticated, allow access to the route
-      next();
-    } else {
-      // User is not authenticated, redirect to login page or send unauthorized response
-      res.status(401).send("Unauthorized");
-    }
-  },
+  
   async (req, res) => {
     try {
       const loggedInUserId = req.session.user._id;
